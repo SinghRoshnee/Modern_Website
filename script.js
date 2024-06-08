ed898a106ffc830b09302a92cd15e8d21bc4e05f
SinghRoshnee <singhroshni012001@gmail.com>
2024-06-08 20:56:39 +0530

    updated

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
-
-
 const sun_color = document.querySelector("#toggledark");
-sun_color.onclick = function() {
-    document.body.classList.toggle("lighttheme");
-    if (document.body.classList.contains("lighttheme")) {
-        sun_color.classList.remove("fa-sharp", "fa-regular", "fa-sun");
-        sun_color.classList.add("fa-solid", "fa-moon");
-    } else {
-        sun_color.classList.remove("fa-solid", "fa-moon");
-        sun_color.classList.add("fa-sharp", "fa-regular", "fa-sun");
-    }
-}
+sun_color.onclick = function () {
+  document.body.classList.toggle("lighttheme");
+  if (document.body.classList.contains("lighttheme")) {
+    sun_color.classList.remove("fa-sharp", "fa-regular", "fa-sun");
+    sun_color.classList.add("fa-solid", "fa-moon");
+  } else {
+    sun_color.classList.remove("fa-solid", "fa-moon");
+    sun_color.classList.add("fa-sharp", "fa-regular", "fa-sun");
+  }
+};
